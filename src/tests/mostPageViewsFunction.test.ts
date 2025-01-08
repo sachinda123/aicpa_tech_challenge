80549f9613ac67e83bafdc9794b6c9d1d13dffbe
sachinda <sachinda@parkwaylabs.com>
2025-01-08 08:56:19 +0530

    on work

--- src/tests/mostPageViewsFunction.test.ts
+++ src/tests/mostPageViewsFunction.test.ts
@@ -1,7 +1,7 @@
-import { readFile } from "../common/functions";
+import { readFile, getPageViewsCount } from "../common/functions";
 import path from "path";
 
-describe("readFile function test", () => {
+describe(" ReadFile function test", () => {
   test("Check file reder skip empty lines", () => {
     try {
       const filePath = path.join(process.cwd(), "src", "./tests/sample/withEmptyLine.log");
@@ -29,6 +29,44 @@ describe("readFile function test", () => {
       const result = readFile(filePath);
       expect.assertions(1);
       expect(result.length).toBe(20000);
+    } catch (error) {
+      console.log("error", error);
+    }
+  });
+});
+
+describe("GetPageViewsCount function test", () => {
+  test("Check log file with empty and non support records ->  /order check/ page names and count check /unsupport log line skip check", () => {
+    try {
+      const filePath = path.join(process.cwd(), "src", "./tests/sample/withEmptyLine.log");
+      const result = readFile(filePath);
+      const count = getPageViewsCount(result);
+
+      expect(count[0].pageName).toBe("/products/3");
+      expect(count[0].totalViews).toBe(4);
+
+      expect(count[1].pageName).toBe("/contact");
+      expect(count[1].totalViews).toBe(4);
+
+      expect(count[2].pageName).toBe("/home");
+      expect(count[2].totalViews).toBe(3);
+
+      expect(count[3].pageName).toBe("/products/1");
+      expect(count[3].totalViews).toBe(2);
+
+      expect(count[4].pageName).toBe("/products");
+      expect(count[4].totalViews).toBe(2);
+
+      expect(count[5].pageName).toBe("/products/2");
+      expect(count[5].totalViews).toBe(1);
+
+      expect(count[6].pageName).toBe("/about");
+      expect(count[6].totalViews).toBe(1);
+
+      expect(count[7].pageName).toBe("/index");
+      expect(count[7].totalViews).toBe(1);
+
+      expect(count.length).toBe(8);
     } catch (error) {
       console.log("error", error);
     }
